0a6bf7293e7c333f03d751b03d8fd1c71273b38e
jornsky <1326820317@qq.com>
2021-02-22 18:43:22 +0800

    Test

---
+++ src/main/java/com/jifeng/fitness/controller/Test.java
@@ -0,0 +1,16 @@
+package com.jifeng.fitness.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test {
+    @RequestMapping("/column")
+    public  String test(Model model){
+//        model.addAttribute("msg","hello");
+        return "column";
+
+    }
+}
